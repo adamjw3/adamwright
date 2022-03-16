108010c97a4ac46dd493c7b1b5d6f6a1405db0b6
Adam Wright <adam.wright90@yahoo.co.uk>
2022-03-16 22:13:46 +0000

    Update Locoscroll.tsx

--- components/LocoScroll/Locoscroll.tsx
+++ components/LocoScroll/Locoscroll.tsx
@@ -4,7 +4,7 @@ import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 const Locoscroll = ({ children }) => {
     const containerRef = useRef(null);
     return (
-        <LocomotiveScrollProvider optiona={{ smooth: true, lerp: 0.5 }} containerRef={containerRef} watch={[]}>
+        <LocomotiveScrollProvider options={{ smooth: true, lerp: 0.5 }} containerRef={containerRef} watch={[]}>
             <div data-scroll-container ref={containerRef} id="scroll-container">
                 <div data-scroll-section>{children}</div>
             </div>
